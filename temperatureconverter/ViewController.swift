ea5f5d0b9870bdcb084b2515fca63304a36a0c27
Meet <pandyama2@gmail.com>
2021-01-02 16:09:37 -0500

    iOS Temperature Converter app
    Slider makes it convenient to convert your ideal temperature values.

--- temperatureconverter/ViewController.swift
+++ temperatureconverter/ViewController.swift
@@ -9,11 +9,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var appLabel: UILabel!
+    @IBOutlet weak var fahrenheitLabel: UILabel!
+    @IBOutlet weak var celsiusLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        appLabel.layer.cornerRadius = 5
+        appLabel.layer.masksToBounds = true
+        
+        fahrenheitLabel.layer.cornerRadius = 8
+        fahrenheitLabel.layer.masksToBounds = true
+        
+        celsiusLabel.layer.cornerRadius = 8
+        celsiusLabel.layer.masksToBounds = true
     }
 
 
+    @IBAction func tempSlider(_ sender: UISlider) {
+        let sliderValue = Double(sender.value)
+        let celsius = sliderValue.rounded()
+        let fahrenheit = ((celsius*(1.8))+32).rounded()
+        
+        let tempC = Int(celsius)
+        let tempF = Int(fahrenheit)
+        
+        fahrenheitLabel.text = String(tempF)
+        celsiusLabel.text = String(tempC)
+    }
+    
 }
 
